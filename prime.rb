bb276ed579a2d194c3a3b398321cdd006779982a
Dan-Vy Le <danvyle85@gmail.com>
2019-05-14 19:36:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
-array = [2, 3, 4, 5, 6, 7, 8, 9]
-
-def prime?(number)
-  each.do |i|
-    (number % array[i]) != 0 
-    i += 1 
+def prime?(num)
+  false if num < 2
+  (2...num).each do |factor|
+    if (num % factor) == 0
+      return false
+    end
+  end
+  true 
 end
-
 #prime numbers are divisible by 1 and itself, but no other numbers.
