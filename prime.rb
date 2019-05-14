5187939a0f7e5d701761d4ff7ddc0bd3332cc83e
Dan-Vy Le <danvyle85@gmail.com>
2019-05-14 19:41:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
-def prime?(num)
-  false if num < 2
-  (2...num).each do |factor|
-    if (num % factor) == 0
-      return false
+
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
     end
+  else
+    return false
   end
-  true 
 end
+
 #prime numbers are divisible by 1 and itself, but no other numbers.
